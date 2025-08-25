e6de507d6ffb646bc956ca94e431af1a09024188
Kasem1509 <iamkasem88@gmail.com>
2025-08-25 12:40:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let username = prompt("Enter Name : \"Kasem or Esraa:");
+switch(username){
+    case "Kasem":
+        document.getElementById("name").innerHTML =
+        "\""+ username +"\"" + " Say's Love You Esraa !";
+        break;
+    case "Esraa":
+        document.getElementById("name").innerHTML =
+        "Hello\"" + username + "\" I Love You  !";
+        break;
+        default:
+        alert("You Entered a wrong name");
+            
+}
